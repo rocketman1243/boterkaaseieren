d27450cf50290ff01dbbf9b3cb31fd6ce119f3e6
Timon Bestebreur <timonbestebreur@gmail.com>
2019-07-15 17:22:34 +0200

    Add usernames, explanation, reformatting
    Also, the board now consists of - instead of 0 initially

--- hi.py
+++ hi.py
@@ -1,10 +1,28 @@
 class Board:
     def __init__(self):
-        self.board = [[0,0,0],[0,0,0],[0,0,0]]
+        self.board = [["-","-","-"],["-","-","-"],["-","-","-"]]
 
     def getData(self):
-        print("{0} {1} {2} \n{3} {4} {5} \n{6} {7} {8}".format(self.board[0][0],self.board[0][1],self.board[0][2],self.board[1][0],self.board[1][1],self.board[1][2],self.board[2][0],self.board[2][1],self.board[2][2]))
+        print("{0} {1} {2} \n{3} {4} {5} \n{6} {7} {8}".format(
+                   self.board[0][0],
+                   self.board[0][1],
+                   self.board[0][2],
+                   self.board[1][0],
+                   self.board[1][1],
+                   self.board[1][2],
+                   self.board[2][0],
+                   self.board[2][1],
+                   self.board[2][2])
+        )
+
+print("Welcome to Boter, Kaas en Eieren. To play, first enter two user names.")
+u1 = input("user 1 (who will play as X:")
+u2 = input("user 2 (who will play as O:")
+
+print("Enter two coordinates (x,y, 0-indexed) to place a marker.")
 
 
 b1 = Board()
 b1.getData()
+
+
