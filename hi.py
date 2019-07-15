0240197e92180f31642a7274e484afd125187efd
Timon Bestebreur <timonbestebreur@gmail.com>
2019-07-15 17:39:10 +0200

    Add basic game loop

--- hi.py
+++ hi.py
@@ -12,17 +12,33 @@ class Board:
                    self.board[1][2],
                    self.board[2][0],
                    self.board[2][1],
-                   self.board[2][2])
-        )
+                   self.board[2][2]
+        )) 
 
 print("Welcome to Boter, Kaas en Eieren. To play, first enter two user names.")
 u1 = input("user 1 (who will play as X:")
 u2 = input("user 2 (who will play as O:")
 
-print("Enter two coordinates (x,y, 0-indexed) to place a marker.")
-
-
 b1 = Board()
+print("Initial board:")
 b1.getData()
 
+print("Enter two coordinates (x,y, 0-indexed) to place a marker.")
 
+finished = False
+while (not finished):
+    u1x = input("User 1 x:")
+    u1y = input("User 1 y:")
+   
+    # b1 = updateBoard(u1x, u1y)
+    # finished = check(b1)
+
+    b1.getData()
+
+    u2x = input("User 2 x:")
+    u2y = input("User 2 y:")
+
+    # b1 = updateBoard(u2x, u2y)
+    # finished = check(b1)
+    
+    b1.getData()
